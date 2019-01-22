6b6811e76bb6850dc65e12ccfaaf82bd0bb6d622
Berrei <46518570+Berrei@users.noreply.github.com>
2019-01-22 23:48:06 +0530

    Add files via upload

---
+++ angle.py
@@ -0,0 +1,6 @@
+x=int(input('Enter the time shown in hours'))
+y=int(input('Enter the time shown in minutes'))
+z=int(input('Enter the time shown in seconds'))
+angle=x*30+y*0.5+z*0.083
+print('The angle between the hour hand and the vertical is',angle)
+input('press enter to exit')
